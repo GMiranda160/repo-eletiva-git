cf6cef31d11bb7f3588d57b172482ee016e906e9
GMiranda160 <gmiranda160@gmail.com>
2026-09-03 11:25:40 -0300

    bugfix de divisao

--- teste.php
+++ teste.php
@@ -4,6 +4,7 @@
     echo "Hoje Tem";
 
     function divisao($num1, $num2){
-        return ($num1 / $num2);
+        if($num2 != 0) return($num1 / $num2);
+        else return("ERROR!");
     }
 ?>
